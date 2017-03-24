ad57e405923e0a96a112320ebdca293c807bc761
Miloszex <miloszex@miloszex>
2017-03-24 20:23:22 +0100

    extendeduser zawiera meetode str

--- account/models.py
+++ account/models.py
@@ -16,3 +16,6 @@ class ExtendedUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     index_number = models.CharField(max_length=6, blank=True)
     year = models.IntegerField(choices=YEAR_CHOICE, default=0)
+
+    def __str__(self):
+        return self.user.username + ' - ' + self.index_number 
