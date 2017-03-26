8d69fd1b52ff4d7b19f0964ca050eb0c994b6a8a
Miloszex <miloszex@miloszex>
2017-03-26 20:25:18 +0200

    naprawiony link /

--- Zaliczenie/urls.py
+++ Zaliczenie/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('signs.urls')),
     url(r'^account/', include('account.urls')),
     url(r'^sign/', include('signs.urls')),
 ]
